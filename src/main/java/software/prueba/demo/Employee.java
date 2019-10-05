1f37789c8b9cb1492d292be20e72904a177d4b93
DANITZA18 <danitzamarlenechurasocujayo@gmail.com>
2019-10-05 11:27:44 -0400

    creacion de la clase employee

--- src/main/java/software/prueba/demo/Employee.java
+++ src/main/java/software/prueba/demo/Employee.java
@@ -1,4 +1,23 @@
 package software.prueba.demo;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Employee {
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    Employee() {}
+
+    Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
 }
