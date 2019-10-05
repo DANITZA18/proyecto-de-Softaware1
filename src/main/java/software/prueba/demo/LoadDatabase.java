b96ade7d89beee487acc9b86ba88297143a317d9
Jimena Choque <jimenachoque.bc@gmail.com>
2019-10-05 18:18:48 -0400

    Añadiendo res

--- src/main/java/software/prueba/demo/LoadDatabase.java
+++ src/main/java/software/prueba/demo/LoadDatabase.java
@@ -1,4 +1,20 @@
 package software.prueba.demo;
 
-public class LoadDatabase {
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Juan Perez", "administrador")));
+            log.info("Preloading " + repository.save(new Employee("Pepe Rayuela", "secretario")));
+        };
+    }
 }
